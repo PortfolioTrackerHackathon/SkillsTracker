6f0e24f30c053b6c3b66d4454c3a91f7024872c8
wadzanayitanhuke411 <wadzanayitanhuke411@gmail.com>
2026-09-04 22:58:52 +0200

    Initial Commit

---
+++ SkillsandPortfolioTracker/SkillsandPortfolioTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillsandPortfolioTrackerApp.swift
+//  SkillsandPortfolioTracker
+//
+//  Created by wadzie on 4/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct SkillsandPortfolioTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
