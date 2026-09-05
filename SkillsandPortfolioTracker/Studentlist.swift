e4789fa68852df999bd5e3b5ce4ab9fd547e8e64
sebastianngwenya90-jpg <sebastianngwenya90@gmail.com>
2026-09-05 05:43:01 +0200

    * Created Student model conforming to Identifiable * Added student names to the student list * Added navigation links to each student's profile * Added student list navigation title

---
+++ SkillsandPortfolioTracker/Studentlist.swift
@@ -0,0 +1,18 @@
+//
+//  Studentlist.swift
+//  SkillsandPortfolioTracker
+//
+//  Created by Sebastian on 5/9/2026.
+//
+
+import SwiftUI
+
+struct Studentlist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Studentlist()
+}
